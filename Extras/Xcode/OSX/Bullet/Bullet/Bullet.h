7ed76540dad077a0764b2d4f5d346f97a49c6fed
Brent Gulanowski <bgulanowski@gmail.com>
2013-06-20 17:22:11 -0600

    Create a native Cocoa framework for Bullet.

---
+++ Extras/Xcode/OSX/Bullet/Bullet/Bullet.h
@@ -0,0 +1,11 @@
+//
+//  Bullet.h
+//  Bullet
+//
+//  Created by Brent Gulanowski on 2013-06-20.
+//  Copyright (c) 2013 Lichen Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
---
+++ Extras/Xcode/OSX/Bullet/BulletTests/BulletTests.m
@@ -0,0 +1,36 @@
+//
+//  BulletTests.m
+//  BulletTests
+//
+//  Created by Brent Gulanowski on 2013-06-20.
+//  Copyright (c) 2013 Lichen Labs. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BulletTests : XCTestCase
+
+@end
+
+@implementation BulletTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
