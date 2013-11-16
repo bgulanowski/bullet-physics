6166ba8ecd25d0a42c7811bd4f2c50f46196ab99
Brent Gulanowski <brent@shopify.com>
2013-11-16 09:39:54 -0700

    New wrapper file.

---
+++ Extras/Xcode/OSX/Bullet/Bullet/BTDiscreteDynamicsWorld.h
@@ -0,0 +1,13 @@
+//
+//  BTDiscreteDynamicsWorld.h
+//  Bullet
+//
+//  Created by Brent Gulanowski on 2013-06-20.
+//  Copyright (c) 2013 Lichen Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BTDiscreteDynamicsWorld : NSObject
+
+@end
---
+++ Extras/Xcode/OSX/Bullet/Bullet/BTDiscreteDynamicsWorld.mm
@@ -0,0 +1,13 @@
+//
+//  BTDiscreteDynamicsWorld.m
+//  Bullet
+//
+//  Created by Brent Gulanowski on 2013-06-20.
+//  Copyright (c) 2013 Lichen Labs. All rights reserved.
+//
+
+#import "BTDiscreteDynamicsWorld.h"
+
+@implementation BTDiscreteDynamicsWorld
+
+@end
